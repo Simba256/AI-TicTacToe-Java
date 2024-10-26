9df39c1b55b8957584011e97c2bc1f561cf6ddea
Syed Basim Mehmood <99350933+Simba256@users.noreply.github.com>
2024-10-26 16:26:13 +0500

    Add files via upload

---
+++ Location.java
@@ -0,0 +1,189 @@
+//KIT107 Assignment 3
+/**
+ *	Location ADT
+ *
+ *	@author <<Your names and Student IDs>>
+ *	@version September 2024
+ *	
+ *	This file holds the Location ADT which represents
+ *	indices to (positions within) the two-dimensional grid.
+ *	A Location consists of a Row number and a Column number.
+ *	
+ *	YOU NEED TO MAKE CHANGES TO THIS FILE!
+*/
+
+
+public class Location implements LocationInterface, Cloneable
+{
+	//finals
+	protected final boolean TRACING=false;				// do we want to see trace output?
+
+	// properties
+	protected int row;			// the vertical component of the position
+	protected int column;		// the horizontal component of the position
+	
+	
+	/**
+	 *	Location
+	 *	Constructor method.
+	 *	Pre-condition: none
+	 *	Post-condition: the Location object records the vertical and
+	 *					horizontal positions within instance variables
+	 *	Informally: intialises the instance variables of the newly
+	 *				created Location object to hold the vertical and
+	 *				horizontal component values
+	 *
+	 *	@param r the row number
+	 *	@param c the column number
+	*/
+	public Location(int r, int c)
+	{
+      	trace("Location: Constructor starts");
+
+		this.row = r;          // Set the row for the location
+    	this.column = c;       // Set the column for the location
+
+      	trace("Location: Constructor ends");
+	}
+	
+	
+	/**
+	 *	setRow
+	 *	Set method for "row" instance variable.
+	 *	Pre-condition: none
+	 *	Post-condition: the Location object's vertical index is
+	 *					altered to hold the given (r) value
+	 *	Informally: assign the value of the parameter to the Location
+	 *				object's vertical component instance variable
+	 *
+	 *	@param r the row number
+	*/
+	public void setRow(int r)
+	{
+      	trace("setRow: setRow starts");
+
+    	this.row = r;          // Update the row value of this location
+
+      	trace("setRow: setRow ends");
+	}
+	
+	
+	/**
+	 *	setColumn
+	 *	Set method for "column" instance variable.
+	 *	Pre-condition: none
+	 *	Post-condition: the Location object's horizontal index is
+	 *					altered to hold the given (c) value
+	 *	Informally: assign the value of the parameter to the Location
+	 *				object's horizontal component instance variable
+	 *
+	 *	@param c the column number
+	*/
+	public void setColumn(int c)
+	{
+      	trace("setColumn: setColumn starts");
+
+		this.column = c;       // Update the column value of this location
+
+      	trace("setColumn: setColumn ends");
+	}
+	
+	
+	/**
+	 *	getRow
+	 *	Get method for "row" instance variable.
+	 *	Pre-condition: none
+	 *	Post-condition: the Location object's vertical index is
+	 *					returned
+	 *	Informally: examine the Location object's vertical component
+	 *				instance variable returning its value
+	 *
+	 *	@return int the row number
+	*/
+	public int getRow()
+	{
+      	trace("getRow: getRow starts and ends");
+
+		return row;            // Return the current row value
+	}
+	
+	
+	/**
+	 *	getColumn
+	 *	Get method for "column" instance variable.
+	 *	Pre-condition: none
+	 *	Post-condition: the Location object's horizontal index is
+	 *					returned
+	 *	Informally: examine the Location object's horizontal component
+	 *				instance variable returning its value
+	 *
+	 *	@return int the column number
+	*/
+	public int getColumn()
+	{
+      	trace("getColumn: getColumn starts and ends");
+
+		return column;         // Return the current column value
+	}
+	
+	
+	/**
+	 *	clone
+	 *	Clone variable for Location objects.
+	 *	Pre-condition: none
+	 *	Post-condition: a new Location object that is a copy of the
+	 *					current object is returned
+	 *	Informally: create a copy of the current object
+	 *
+	 *	@return Object the copied Location
+	*/
+	public Object clone()
+	{
+		Location l;
+		
+      	trace("clone: clone starts");
+
+		l = new Location(this.row, this.column);  // Create a new Location with the same row and column
+
+      	trace("clone: clone ends");
+		return l;
+	}
+	
+	
+	/**
+	 *	toString
+	 *	Convert the location to a String.
+	 *	Pre-condition: none
+	 *	Post-condition: a String representation of the current
+	 *					Location is returned
+	 *	Informally: find the String equivalent of the current
+	 *				location
+	 *
+	 *	@return String the printable form of the Location
+	*/
+	public String toString()
+	{
+      	trace("toString: toString starts and ends");
+      	
+		return ("row " + getRow() + " and column " + getColumn());
+	}
+
+
+	/**
+	 *	trace
+	 *	Provide trace output.
+	 *	Pre-condition: none
+	 *	Post-condition: if trace output is desired then the given String
+	 *					parameter is shown on the console
+	 *	Informally: show the given message for tracing purposes
+	 *
+	 *	@param s String to display as a trace message
+	*/
+	protected void trace(String s)
+	{
+		if (TRACING)
+		{
+			System.out.println("Location: " + s);
+		}
+	}
+}
---
+++ Stack.java
@@ -0,0 +1,232 @@
+//KIT107 Assignment 3
+/**
+ *	Stack ADT
+ *
+ *	@author <<Your names and Student IDs>>
+ *	@version September 2024
+ *	
+ *	This file holds the Stack ADT.  The Stack is built
+ *	using a linked list of Node ADTs.  A Stack object
+ *	consists of a "tos" field which refers to a Node
+ *	object.
+ *	
+ *	YOU NEED TO MAKE CHANGES TO THIS FILE!
+*/
+
+
+public class Stack implements StackInterface
+{
+	//finals
+	protected final boolean TRACING=false;				// do we want to see trace output?
+
+	// properties
+	protected Node tos;		// the node on the top of the stack
+
+
+	/**
+	 *	Stack
+	 *	Constructor method 1.
+	 *	Pre-condition: none
+	 *	Post-condition: the Stack object's "tos" field is null
+	 *	Informally: intialises the instance variable of the newly
+	 *				created Stack object by terminating the "tos"
+	 *				field
+	*/
+	public Stack()
+	{
+      	trace("Stack: Constructor ends");
+
+		this.tos=null;
+
+      	trace("Stack: Constructor ends");
+	}
+
+
+	/**
+	 *	Stack
+	 *	Constructor method 2.
+	 *	Pre-condition: none
+	 *	Post-condition: the Stack object holds a reference to a Node
+	 *					object within its "tos" field which holds
+	 *					the parameter value (o)
+	 *	Informally: intialises the instance variable ("tos") of the
+	 *				newly created Stack object to hold a reference to
+	 *				a new Node object containing the given parameter
+	 *
+	 *	@param o the Object to store within the Stack
+	*/
+	public Stack(Object o)
+	{
+      	trace("Stack: Constructor ends");
+
+		this.tos=new Node(o);
+
+      	trace("Stack: Constructor ends");
+	}
+	
+	
+	/**
+	 *	isEmpty
+	 *	Check whether the stack is empty or not.
+	 *	Pre-condition: none
+	 *	Post-condition: true is returned if the Stack object has no
+	 *					items, false is returned if it does
+	 *	Informally: examine the Stack object's tos instance variable
+	 *					returning whether or not it is null
+	 *
+	 *	@return boolean whether or not the Stack is empty
+	*/
+	public boolean isEmpty()
+	{
+      	trace("isEmpty: isEmpty starts and ends");
+
+		if(this.tos == null) return true; // Check if top of stack is null, indicating an empty stack
+
+		return false;                     // Otherwise Stack is not empty
+	}
+
+
+	/**
+	 *	top
+	 *	Examine the top item on the stack.
+	 *	Pre-condition: the stack is non-empty
+	 *	Post-condition: a reference to the item on the top of the
+	 *					stack is returned
+	 *	Informally: return a reference to the top of the Stack or
+	 *				throw an exception if the Stack is empty
+	 *
+	 *	@return Object the item at the top of the Stack
+	*/
+	public Object top() throws EmptyStackException
+	{
+      	trace("top: top starts");
+
+		if (isEmpty())
+		{
+	      	trace("top: empty stack");
+			throw new EmptyStackException();
+		}
+		else
+		{
+
+			trace("top: top ends");
+			return this.tos.getData();        // Return data of the top element in the stack
+		}
+	}
+
+
+	/**
+	 *	pop
+	 *	Delete the top item from the stack.
+	 *	Pre-condition: the stack is non-empty
+	 *	Post-condition: the "tos" instance variable is re-assigned to
+	 *					refer to the value of the second Node object
+	 *					in the linked list
+	 *	Informally: bypass the node on the top of the Stack so that
+	 *				the second node on the Stack becomes the top.  If
+	 *				the Stack is empty, throw an exception
+	*/
+	public void pop() throws EmptyStackException
+	{
+      	trace("pop: pop starts");
+
+		if (isEmpty())
+		{
+ 	     	trace("pop: empty stack");
+			throw new EmptyStackException();
+		}
+		else
+		{
+	      	trace("pop: adjusting top of stack");
+			this.tos = this.tos.getNext();    // Remove the top element by updating tos to the next node
+		}
+		
+      	trace("pop: pop ends");
+	}
+
+
+	/**
+	 *	push
+	 *	Add an item onto the stack.
+	 *	Pre-condition: none
+	 *	Post-condition: the Stack object's instance variable "tos" is
+	 *					made to refer to a new Node object containing
+	 *					the parameter (o) and the Node's "next" field
+	 *					is assigned the original value of the "tos"
+	 *					instance variable
+	 *	Informally: create a new Node and insert it	before the others
+	 *				on the stack making it the new top of stack
+	 *
+	 *	@param o the Object to add to the top of the Stack
+	*/
+	public void push(Object o)
+	{
+		Node n = new Node(o);
+		
+      	trace("push: push starts");
+      	
+		n.setNext(this.tos);              // Link new node to the current top of stack
+
+		this.tos = n;                     // Set new node as the top of stack
+
+      	trace("push: push ends");
+	}
+	
+	
+	/**
+	 *	toString
+	 *	Convert the contents of the stack to a printable representation.
+	 *	Pre-condition: none
+	 *	Post-condition: a String object is returned consisting of the
+	 *				String representation of all items on the Stack,
+	 *				from top to bottom in order, separated by " " and
+	 *				contained within "<" and ">"
+	 *	Informally: produce a String representation of the Stack
+	 *
+	 *	@return String the printable representation of the contents of
+	 *				the Stack
+	*/
+	public String toString()
+	{
+		Node c;
+		String s="";
+		
+      	trace("toString: toString starts");
+
+		if (isEmpty())
+		{
+  	    	trace("toString: toString ends empty");
+			s="<>";
+		}
+		else
+		{
+			c=tos;
+			while (c != null)
+			{
+				s+=(c.getData().toString() + " ");
+				c=c.getNext();
+			}
+	      	trace("toString: toString ends");
+		}
+		return s;
+	}
+
+
+	/**
+	 *	trace
+	 *	Provide trace output.
+	 *	Pre-condition: none
+	 *	Post-condition: if trace output is desired then the given String
+	 *					parameter is shown on the console
+	 *	Informally: show the given message for tracing purposes
+	 *
+	 *	@param s the String to be displayed as the trace message
+	*/
+	protected void trace(String s)
+	{
+		if (TRACING)
+		{
+			System.out.println("Stack: " + s);
+		}
+	}
+}
